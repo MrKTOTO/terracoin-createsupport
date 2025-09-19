bada6a3e9ea0b42bca822dfe5a531a6efa8f4c9a
WhoXho <55439592+MrKTOTO@users.noreply.github.com>
2025-09-19 18:41:54 +0300

    Update mining.cpp

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -1157,70 +1157,67 @@ static UniValue AuxMiningCreateBlock(const CScript& scriptPubKey)
     static std::map<uint256, CBlock*> mapNewBlock;
     static std::vector<CBlockTemplate*> vNewBlockTemplate;
 
-    /* Create a new block?  */
-    if (params.size() == 0)
-    {
-        static unsigned nTransactionsUpdatedLast;
-        static const CBlockIndex* pindexPrev = NULL;
-        static uint64_t nStart;
-        static CBlockTemplate* pblocktemplate;
-        static unsigned nExtraNonce = 0;
+    static unsigned nTransactionsUpdatedLast;
+    static const CBlockIndex* pindexPrev = NULL;
+    static uint64_t nStart;
+    static CBlockTemplate* pblocktemplate;
+    static unsigned nExtraNonce = 0;
 
-        // Update block
+    // Update block
+    {
+        LOCK(cs_main);
+        if (pindexPrev != chainActive.Tip()
+            || (mempool.GetTransactionsUpdated() != nTransactionsUpdatedLast
+                && GetTime() - nStart > 60))
         {
-            LOCK(cs_main);
-            if (pindexPrev != chainActive.Tip()
-                || (mempool.GetTransactionsUpdated() != nTransactionsUpdatedLast
-                    && GetTime() - nStart > 60))
+            if (pindexPrev != chainActive.Tip())
             {
-                if (pindexPrev != chainActive.Tip())
-                {
-                    // Deallocate old blocks since they're obsolete now
-                    mapNewBlock.clear();
-                    BOOST_FOREACH(CBlockTemplate* pbt, vNewBlockTemplate)
-                        delete pbt;
-                    vNewBlockTemplate.clear();
-                }
-        
-                // Create new block with nonce = 0 and extraNonce = 1
-                pblocktemplate = CreateNewBlock(Params(), scriptPubKey);
-                if (!pblocktemplate)
-                    throw JSONRPCError(RPC_OUT_OF_MEMORY, "out of memory");
-        
-                // Update state only when CreateNewBlock succeeded
-                nTransactionsUpdatedLast = mempool.GetTransactionsUpdated();
-                pindexPrev = chainActive.Tip();
-                nStart = GetTime();
-        
-                // Finalise it by setting the version and building the merkle root
-                CBlock* pblock = &pblocktemplate->block;
-                IncrementExtraNonce(pblock, pindexPrev, nExtraNonce);
-                pblock->SetAuxpowVersion(true);
-        
-                // Save
-                mapNewBlock[pblock->GetHash()] = pblock;
-                vNewBlockTemplate.push_back(pblocktemplate);
+                // Deallocate old blocks since they're obsolete now
+                mapNewBlock.clear();
+                BOOST_FOREACH(CBlockTemplate* pbt, vNewBlockTemplate)
+                    delete pbt;
+                vNewBlockTemplate.clear();
             }
+    
+            // Create new block with nonce = 0 and extraNonce = 1
+            pblocktemplate = CreateNewBlock(Params(), scriptPubKey);
+            if (!pblocktemplate)
+                throw JSONRPCError(RPC_OUT_OF_MEMORY, "out of memory");
+    
+            // Update state only when CreateNewBlock succeeded
+            nTransactionsUpdatedLast = mempool.GetTransactionsUpdated();
+            pindexPrev = chainActive.Tip();
+            nStart = GetTime();
+    
+            // Finalise it by setting the version and building the merkle root
+            CBlock* pblock = &pblocktemplate->block;
+            IncrementExtraNonce(pblock, pindexPrev, nExtraNonce);
+            pblock->SetAuxpowVersion(true);
+    
+            // Save
+            mapNewBlock[pblock->GetHash()] = pblock;
+            vNewBlockTemplate.push_back(pblocktemplate);
         }
+    }
         
-        const CBlock& block = pblocktemplate->block;
-        
-        arith_uint256 target;
-        bool fNegative, fOverflow;
-        target.SetCompact(block.nBits, &fNegative, &fOverflow);
-        if (fNegative || fOverflow || target == 0)
-            throw std::runtime_error("invalid difficulty bits in block");
-        
-        UniValue result(UniValue::VOBJ);
-        result.push_back(Pair("hash", block.GetHash().GetHex()));
-        result.push_back(Pair("chainid", block.GetChainId()));
-        result.push_back(Pair("previousblockhash", block.hashPrevBlock.GetHex()));
-        result.push_back(Pair("coinbasevalue", (int64_t)block.vtx[0].vout[0].nValue));
-        result.push_back(Pair("bits", strprintf("%08x", block.nBits)));
-        result.push_back(Pair("height", static_cast<int64_t> (pindexPrev->nHeight + 1)));
-        result.push_back(Pair("target", HexStr(BEGIN(target), END(target))));
-        
-        return result;
+    const CBlock& block = pblocktemplate->block;
+    
+    arith_uint256 target;
+    bool fNegative, fOverflow;
+    target.SetCompact(block.nBits, &fNegative, &fOverflow);
+    if (fNegative || fOverflow || target == 0)
+        throw std::runtime_error("invalid difficulty bits in block");
+    
+    UniValue result(UniValue::VOBJ);
+    result.push_back(Pair("hash", block.GetHash().GetHex()));
+    result.push_back(Pair("chainid", block.GetChainId()));
+    result.push_back(Pair("previousblockhash", block.hashPrevBlock.GetHex()));
+    result.push_back(Pair("coinbasevalue", (int64_t)block.vtx[0].vout[0].nValue));
+    result.push_back(Pair("bits", strprintf("%08x", block.nBits)));
+    result.push_back(Pair("height", static_cast<int64_t> (pindexPrev->nHeight + 1)));
+    result.push_back(Pair("target", HexStr(BEGIN(target), END(target))));
+    
+    return result;
 }
 
 static bool AuxMiningSubmitBlock(const std::string& hashHex, const std::string& auxpowHex)
